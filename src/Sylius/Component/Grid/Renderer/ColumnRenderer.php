121a76a927935affbb155cc6240a981c14338c37
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2015-02-22 23:54:15 +0100

    WIP

---
+++ src/Sylius/Component/Grid/Renderer/ColumnRenderer.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Component\Grid\Renderer;
+
+use Sylius\Component\Grid\Definition\Column;
+use Sylius\Component\Registry\ServiceRegistryInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @author Paweł Jędrzejewski <pawel@sylius.org>
+ */
+class ColumnRenderer implements ColumnRendererInterface
+{
+    /**
+     * @var ServiceRegistryInterface
+     */
+    private $columnTypeRegistry;
+
+    /**
+     * @var array
+     */
+    private $resolvedOptions = array();
+
+    /**
+     * @param ServiceRegistryInterface $filterRegistry
+     */
+    public function __construct(ServiceRegistryInterface $columnTypeRegistry)
+    {
+        $this->columnTypeRegistry = $columnTypeRegistry;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render($data, Column $column)
+    {
+        return $this->columnTypeRegistry->get($column->getType())->render($data, $column->getName(), $this->resolveOptions($column));
+    }
+
+    /**
+     * @param Column $column
+     *
+     * @return array
+     */
+    private function resolveOptions(Column $column)
+    {
+        $type = $column->getType();
+
+        if (isset($this->resolvedOptions[$type])) {
+            return $this->resolvedOptions[$type];
+        }
+
+        $optionsResolver = new OptionsResolver();
+
+        $this->columnTypeRegistry->get($type)->setOptions($optionsResolver);
+
+        return $this->resolvedOptions[$type] = $optionsResolver->resolve($column->getOptions());
+    }
+}
